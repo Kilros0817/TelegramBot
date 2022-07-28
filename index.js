bef7350c6fc7e215e7eae007417bf0b1f8889e1f
Kilros0817 <lazarkitfrl@gmail.com>
2022-07-29 04:55:51 +0900

    create tgbot no option

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const token = '5315416686:AAFAPv0qJznkMF9KVkzaMW8SQmf1saO7rXI';
 const channel_id = '-1001625382295';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token);
 
 function broadcastMessage(){
     console.log('send message');
-    // bot.sendMessage(channel_id, 'BURN DETECTED');
+    bot.sendMessage(channel_id, 'BURN DETECTED');
 }
 
 setTimeout(broadcastMessage, 2000);
