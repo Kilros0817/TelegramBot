dca57e9d3ed1569cd69b9f91c028abad42291aaa
Kilros0817 <lazarkitfrl@gmail.com>
2022-07-29 07:52:30 +0800

    update code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // replace the value below with the Telegram token you receive from @BotFather
-import Web3 from "web3";
-import Incinerator from "./const/Incinerator.json";
+const Web3 = require("web3");
+const Incinerator = require("./const/Incinerator.json");
 
 const socketProvider =
   "wss://speedy-nodes-nyc.moralis.io/3eb1bbe3cea7ab422badd600/bsc/mainnet/ws";
@@ -8,7 +8,18 @@ const socketProvider =
 const web3Socket = new Web3(
   new Web3.providers.WebsocketProvider(socketProvider)
 );
-const IncineratorContract = new web3Socket.eth.Contract(
+
+const socket = new web3Socket.eth.Contract(
+  Incinerator.abi,
+  Incinerator.contractAddress
+);
+
+const API_URL =
+  "https://speedy-nodes-nyc.moralis.io/3eb1bbe3cea7ab422badd600/bsc/mainnet";
+
+  const web3 = new Web3(new Web3.providers.HttpProvider(API_URL));
+
+const contract = new web3.eth.Contract(
   Incinerator.abi,
   Incinerator.contractAddress
 );
@@ -20,8 +31,10 @@ const channel_id = "-1001616504473";
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-IncineratorContract.events.TokenBurn().on("data", async function (event) {
+socket.events.TokenBurn().on("data", async function (event) {
   let data = event.returnValues;
-    console.log("data", data);
-	bot.sendMessage(channel_id, "BURN DETECTED");
+  console.log("data", data);
+  const supply = await contract.methods.totalSupply().call();
+  console.log("totalSupply", supply);
+  bot.sendMessage(channel_id, "BURN DETECTED");
 });
