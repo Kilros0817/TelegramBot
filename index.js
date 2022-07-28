4f61bb792618751a9453d4da928ac0aa4bb5aa61
Kilros0817 <lazarkitfrl@gmail.com>
2022-07-29 02:11:09 +0800

    update code

--- index.js
+++ index.js
@@ -1,50 +1,8 @@
-var express = require("express")
-var app = express()
-var bodyParser = require("body-parser")
-const axios = require("axios")
+// replace the value below with the Telegram token you receive from @BotFather
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5421835043:AAElpJAls3p3aChs6lH3VtLkelGM66I42y0';
+const channel_id = '-1001616504473';
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
-) // for parsing application/x-www-form-urlencoded
-
-//This is the route the API will call
-app.post("/new-message", function(req, res) {
-	const { message } = req.body
-
-	//Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-
-	if (!message || message.text.toLowerCase().indexOf("panther") < 0) {
-		// In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-		return res.end()
-	}
-
-	// If we've gotten this far, it means that we have received a message containing the word "marco".
-	// Respond by hitting the telegram bot API and responding to the appropriate chat_id with the word "Polo!!"
-	// Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-	axios
-		.post(
-			"https://api.telegram.org/bot5315416686:AAFAPv0qJznkMF9KVkzaMW8SQmf1saO7rXI/sendMessage",
-			{
-				chat_id: message.chat.id,
-				text: "Yeah, I'm Panther.",
-			}
-		)
-		.then((response) => {
-			// We get here if the message was successfully posted
-			console.log("Message posted")
-			res.end("ok")
-		})
-		.catch((err) => {
-			// ...and here if it was not
-			console.log("Error :", err)
-			res.end("Error :" + err)
-		})
-})
-
-// Finally, start our server
-app.listen(3000, function() {
-	console.log("Telegram app listening on port 3000!")
-})
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+setInterval(bot.sendMessage(channel_id, 'BURN DETECTED'), 2000)
