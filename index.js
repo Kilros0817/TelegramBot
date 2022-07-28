5a459c5e3bd1bee2b3ba8a877e033cfa167093b8
Kilros0817 <lazarkitfrl@gmail.com>
2022-07-29 07:21:41 +0900

    merge code5

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // replace the value below with the Telegram token you receive from @BotFather
-// const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 const token = '5315416686:AAFAPv0qJznkMF9KVkzaMW8SQmf1saO7rXI';
 const channel_id = '-1001625382295';
 
 // Create a bot that uses 'polling' to fetch new updates
-// const bot = new TelegramBot(token);
+const bot = new TelegramBot(token);
 
 function broadcastMessage(){
     console.log('send message');
-    // bot.sendMessage(channel_id, 'BURN DETECTED');
+    bot.sendMessage(channel_id, 'BURN DETECTED');
 }
 
 setTimeout(broadcastMessage, 1000);
